1b74f6706704adf92354c7c0225c556bfcf73096
LEE <dmstn356@naver.com>
2023-03-20 15:53:12 +0900

    dividend 수정2

--- src/main/java/com/dayone/service/CompanyService.java
+++ src/main/java/com/dayone/service/CompanyService.java
@@ -3,7 +3,6 @@ package com.dayone.service;
 import com.dayone.exception.impl.NoCompanyException;
 import com.dayone.model.Company;
 import com.dayone.model.ScrapedResult;
-import com.dayone.model.constants.CacheKey;
 import com.dayone.persist.CompanyRepository;
 import com.dayone.persist.DividendRepository;
 import com.dayone.persist.entity.CompanyEntity;
@@ -12,14 +11,11 @@ import com.dayone.scraper.Scraper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.Trie;
-import org.apache.commons.collections4.trie.PatriciaTrie;
 import org.hibernate.cfg.NotYetImplementedException;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ObjectUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -31,26 +27,50 @@ public class CompanyService {
 
     private final Trie trie;
 
+    private final Scraper yahooFinanceScraper;
+
+    private final CompanyRepository companyRepository;
+
+    private final DividendRepository dividendRepository;
+
     public Company save(String ticker) {
-        throw new NotYetImplementedException();
+        boolean exists = this.companyRepository.existsByTicker(ticker);
+        if (exists) {
+            throw new RuntimeException("already exists ticker -> " + ticker);
+        }
+
+        return this.storeCompanyAndDividend(ticker);
     }
 
     public Page<CompanyEntity> getAllCompany(Pageable pageable) {
-        throw new NotYetImplementedException();
+        return this.companyRepository.findAll(pageable);
     }
 
     private Company storeCompanyAndDividend(String ticker) {
         // 1. ticker 를 기준으로 회사를 스크래핑
+        Company company = this.yahooFinanceScraper.scrapCompanyByTicker(ticker);
 
         // 2. 해당 회사가 존재할 경우, 회사의 배당금 정보를 스크래핑
+        ScrapedResult scrapedResult = this.yahooFinanceScraper.scrap(company);
 
         // 3. 스크래핑 결과 반환
+        CompanyEntity companyEntity =
+                this.companyRepository.save(new CompanyEntity(company));
+        List<DividendEntity> dividendEntities = scrapedResult.getDividends().stream()
+                .map(e -> new DividendEntity(companyEntity.getId(), e))
+                .collect(Collectors.toList());
 
-        throw new NotYetImplementedException();
+        this.dividendRepository.saveAll(dividendEntities);
+        return company;
     }
 
     public List<String> getCompanyNamesByKeyword(String keyword) {
-        throw new NotYetImplementedException();
+        Pageable limit = PageRequest.of(0, 10);
+        Page<CompanyEntity> companyEntities =
+                this.companyRepository.findByNameStartingWithIgnoreCase(keyword, limit);
+        return companyEntities.stream()
+                .map(e -> e.getName())
+                .collect(Collectors.toList());
     }
 
     public void addAutocompleteKeyword(String keyword) {
@@ -60,6 +80,7 @@ public class CompanyService {
     public List<String> autocomplete(String keyword) {
         return (List<String>) this.trie.prefixMap(keyword).keySet()
                 .stream()
+                .limit(10)
                 .collect(Collectors.toList());
     }
 
@@ -68,9 +89,16 @@ public class CompanyService {
     }
 
     public String deleteCompany(String ticker) {
+        var company = this.companyRepository.findByTicker(ticker)
+                .orElseThrow(() -> new NoCompanyException());
+
         // 1. 배당금 정보 삭제
+        this.dividendRepository.deleteAllByCompanyId(company.getId());
         // 2. 회사 정보 삭제
-        throw new NotYetImplementedException();
+        this.companyRepository.delete(company);
+
+        this.deleteAutocompleteKeyword(company.getName());
+        return company.getName();
     }
 
 }
